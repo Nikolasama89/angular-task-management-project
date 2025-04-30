734c93cd3bc257c1fa0f30dcdb1ffdfc5cd0985c
Nikos <niqos89@gmail.com>
2025-04-30 11:59:28 +0300

    initial commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: "app-header",
+  // For Angular 19 and higher this is default
+  standalone: true,
+  templateUrl: "./header.component.html",
+  styleUrl: "./header.component.css"
+})
+export class HeaderComponent {}
