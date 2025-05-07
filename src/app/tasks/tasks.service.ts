5198864f380698f91e695752c257fed42f6a8705
Nikos <niqos89@gmail.com>
2025-05-07 14:40:32 +0300

    feat: used localStorage for data storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -30,6 +30,15 @@ export class TasksService {
       },
     ];
 
+    // Constructor to save tasks in local storage
+    constructor() {
+      const tasks = localStorage.getItem("tasks")
+
+      if (tasks) {
+        this.tasks = JSON.parse(tasks);
+      }
+    }
+
     getUserTasks(userId: string) {
       return this.tasks.filter((task) => task.userId === userId)
     }
@@ -41,10 +50,16 @@ export class TasksService {
         title: taskData.title,
         summary: taskData.summary,
         dueDate: taskData.date
-      })
+      });
+      this.saveTasks();
     }
 
     removeTask(id: string) {
       this.tasks = this.tasks.filter((task) => task.id !== id);
+      this.saveTasks();
+    }
+
+    private saveTasks() {
+      localStorage.setItem("tasks", JSON.stringify(this.tasks));
     }
 }
